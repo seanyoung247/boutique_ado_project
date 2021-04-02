e5b941df31f6aed29ad98cc0eb506337a1aa1ea1
seanyoung247 <seanyoung247@gmail.com>
2021-04-02 21:07:31 +0100

    updated context processor

--- bag/views.py
+++ bag/views.py
@@ -7,6 +7,7 @@ def view_bag(request):
 
     return render(request, 'bag/bag.html')
 
+
 def add_to_bag(request, item_id):
     """ Add a quantity of the specified product to the shopping bag """
 
@@ -20,6 +21,4 @@ def add_to_bag(request, item_id):
         bag[item_id] = quantity
 
     request.session['bag'] = bag
-    print(request.session['bag'])
     return redirect(redirect_url)
-    

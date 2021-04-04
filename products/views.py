dffa183f61aeecb24dd7ea21c868ac32ce2c2b6b
seanyoung247 <seanyoung247@gmail.com>
2021-04-04 11:02:55 +0100

    feat: adds notification CSS, shopping bag preview, and additional messages

--- products/views.py
+++ products/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render, redirect, reverse, get_object_or_404
 from django.db.models import Q
 from django.db.models.functions import Lower
+from django.contrib import messages
 from .models import Product, Category
 
 

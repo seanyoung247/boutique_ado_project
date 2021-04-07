6705c0037ab6df6e9af3bd86952d484e1f19a8dd
seanyoung247 <seanyoung247@gmail.com>
2021-04-07 17:03:29 +0100

    feat: completes webhook handler

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,6 @@
 
 from django.shortcuts import render, redirect, reverse, get_object_or_404, HttpResponse
-from django.views.decoractors.http import require_POST
+from django.views.decorators.http import require_POST
 from django.contrib import messages
 from django.conf import settings
 
@@ -11,7 +11,6 @@ from bag.contexts import bag_contents
 
 import stripe
 import json
-
 
 @require_POST
 def cache_checkout_data(request):
@@ -25,8 +24,8 @@ def cache_checkout_data(request):
         })
         return HttpResponse(status=200)
     except Exception as e:
-        messages.error(request, 'Sorry, your payment cannot \
-            be processed right now. Please try again later.')
+        messages.error(request, 'Sorry, your payment cannot be \
+            processed right now. Please try again later.')
         return HttpResponse(content=e, status=400)
 
 
@@ -50,7 +49,11 @@ def checkout(request):
         }
         order_form = OrderForm(form_data)
         if order_form.is_valid():
-            order = order_form.save()
+            order = order_form.save(commit=False)
+            pid = request.POST.get('client_secret').split('_secret')[0]
+            order.stripe_pid = pid
+            order.original_bag = json.dumps(bag)
+            order.save()
             for item_id, item_data in bag.items():
                 try:
                     product = Product.objects.get(id=item_id)

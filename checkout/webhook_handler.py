1ed291bfe7ed3f862297cb9e9514b184c999dd06
seanyoung247 <seanyoung247@gmail.com>
2021-04-07 16:24:29 +0100

    feat: adds checkout form caching of payment intent

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -1,4 +1,3 @@
-
 from django.http import HttpResponse
 
 
@@ -20,6 +19,8 @@ class StripeWH_Handler:
         """
         Handle the payment_intent.succeeded webhook from Stripe
         """
+        intent = event.data.object
+        print(intent)
         return HttpResponse(
             content=f'Webhook received: {event["type"]}',
             status=200)
